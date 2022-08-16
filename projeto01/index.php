5a1db3dd3daed32c19e5353f38cbff91760d60d2
fodase4x <103192798+fodase4x@users.noreply.github.com>
2022-08-16 09:35:57 -0300

    Add files via upload

---
+++ projeto01/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>projeto 01</title>
+	<script src="https://kit.fontawesome.com/d1f38def45.js"crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Oleo+Script+Swash+Caps&display=swap">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="keywords" content="palavra-chave,do,meu,site">
+
+</head>
+	
+<body>
+
+	<header>
+		<div class="center">
+		<div class="logo left">logomarca</div>
+		<nav class="desktop"></nav>
+			<ul>
+				<li><a href="">home</a></li>
+				<li><a href="">sobre</a></li>
+				<li><a href="">serviços</a></li>
+				<li><a href="">contato</a></li>
+			</ul>
+		</nav>
+		<nav class ="mobile right">
+			<ul>
+				<li><a href="">home</a></li>
+				<li><a href="">sobre</a></li>
+				<li><a href="">serviços</a></li>
+				<li><a href="">contato</a></li>
+			</ul>
+		</nav>
+		<div class="clear"></div>
+		</div> <!--center-->
+	</header>
+
+	<section class="banner-principal">
+		<div class="overlay"></div>
+		<div class="center"><!--onde o usuario vai colocar o email -->
+			<form>
+				<h2>qual o seu melhor email?</h2>
+				<input type="email" name="email" required/>
+				<input type="submit" name="acao" value="cadastrar"/>
+			</form>
+		</div>
+	</section>
+
+	<!--descrição do outor-->
+	<section class="descrição-autor">
+		<div class="center">
+		<div class="w50 left"><!--vai pegar 50% da largura da tela-->
+		<h2>eu</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div><!--w50-->
+		<div class="w50% left">
+			<img src="img/f.png"/>
+		</div>
+		</div><!--center-->
+		<div class="clear"></div><!--limpar tela-->
+	</section>
+
+	<section class="especialidades">
+		<div class="center">
+		<h2 class="title">especialidades</h2>
+			<div class="w33 left box-especialidades">
+			<h3><a
+			</div>
+		</div>
+	</section>
+</body>
+</html>
